a71db7e4ecef459ab1d06b4713c422ea1fafbea3
Shivam Raje <shivamraje848@gmail.com>
2024-10-04 22:40:56 +0530

    Changes done

--- Javascript/four.js
+++ Javascript/four.js
@@ -42,3 +42,14 @@
 // // let deletedvalue=foodItems.pop();
 // // console.log(foodItems);
 // // console.log(deletedvalue);
+
+let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(companies);
+companies.shift();
+console.log(companies);
+companies.splice(1,1,"Ola");
+console.log(companies);
+companies.push("Amazon");
+console.log(companies);
+
+
