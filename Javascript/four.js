c3ecdfdac25f8df255897600feb20a3511f55fc3
Shivam Raje <shivamraje848@gmail.com>
2024-10-04 22:09:22 +0530

    Changes done

--- Javascript/four.js
+++ Javascript/four.js
@@ -1,6 +1,44 @@
-let marks=[97,68,67,63,90,78];
+// let marks=[97,68,67,63,90,78];
 
-for(let i =0;i<marks.length;i++){
-    console.log(marks[i]);
-}
+// for(let i =0;i<marks.length;i++){
+//     console.log(marks[i]);
+// }
 
+// for (let item of marks) {
+//     console.log(item);
+    
+// }
+// let cities=["Delhi","Mysore","Mumbai"];
+// for (let city of cities) {
+//     console.log(city.toUpperCase());
+    
+// }
+
+// let marks=[85,97,44,37,76,60];
+// let sum=0;
+// for(let i=0 ;i<marks.length;i++){
+//     sum=sum+marks[i];
+// }
+// let result=sum/marks.length;
+// console.log("Average of marks are : ",result );
+
+// let items=[250,645,300,900,50];
+// for (let i=0;i<items.length;i++) {
+//     let offer=items[i]/10;
+//     items[i]=items[i]-offer;
+//     console.log(`value at index ${i} = ${items[i]}`);
+// }
+
+// let foodItems=["Potato","Tomato","Grapse"];
+// console.log(foodItems);
+// //foodItems.unshift("Banana");
+// let vale=foodItems.shift();
+// console.log(foodItems);
+// console.log(vale);
+// // console.log(foodItems1); 
+// // let three=foodItems.concat(foodItems1);
+// // console.log(three);
+
+// // let deletedvalue=foodItems.pop();
+// // console.log(foodItems);
+// // console.log(deletedvalue);
