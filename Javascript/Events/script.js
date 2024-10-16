c439f7823600d57b24b7195cfb11db5f70350241
Shivam Raje <shivamraje848@gmail.com>
2024-10-16 23:20:43 +0530

    Changes done

---
+++ Javascript/Events/script.js
@@ -0,0 +1,11 @@
+let btn1 =document.querySelector("#btn1");
+btn1.onclick=()=>{
+     console.log("Button was click");
+     let a=25;
+     a++;
+     console.log(a);
+}
+let box=document.querySelector("#box1");
+box.onmouseover=()=>{
+    console.log("You are in box");
+}
